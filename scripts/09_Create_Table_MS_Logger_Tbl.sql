4fef12139a20cc019a18ff55cb92d71e5a80eec8
kaizhou-sg <kai.zhou.sg@gmail.com>
2015-01-23 09:36:34 +0000

    add / delete

---
+++ scripts/09_Create_Table_MS_Logger_Tbl.sql
@@ -0,0 +1,14 @@
+CREATE TABLE MS_Logger_Tbl (
+  Msg_Code NVARCHAR(2) NOT NULL,
+  Msg_Id BIGINT NOT NULL,
+  Sent_Date NVARCHAR(10) NOT NULL,
+  Sent_Time NVARCHAR(4) NOT NULL,
+  Received_Date NVARCHAR(10) NOT NULL,
+  Received_Time NVARCHAR(4) NOT NULL,
+  Sent_By NVARCHAR(10) NOT NULL,
+  Received_By NVARCHAR(10) NOT NULL,
+
+  CONSTRAINT pk_ms_logger PRIMARY KEY (Msg_Code, Msg_Id)
+)
+
+GO
